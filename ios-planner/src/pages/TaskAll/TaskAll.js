fba51be7bc30234ca45854d5f8e4472674ba5ee8
Artem Suhoy <artemsuhoy7@gmail.com>
2021-12-28 18:38:19 +0200

    AllSwitcher

--- ios-planner/src/pages/TaskAll/TaskAll.js
+++ ios-planner/src/pages/TaskAll/TaskAll.js
@@ -2,34 +2,42 @@ import { Row, Col, Header, Title } from "components"
 import {
   TaskList,
   CategoryAdvancedView,
-  TaskAdvancedView,
+  TaskSimpleView,
 } from "domains/Task/Components"
 import { useContext, useState } from "react"
 import { PlannerContext } from "./../../contexts/Planner"
 const TaskAll = props => {
   const { state } = useContext(PlannerContext)
+
   const AllTasks = state => {
     const result = state.categories.map(category => {
       let categoryId = category.taskListId
       let categoryName = category.taskListName
-      return state.tasks.map(
-        task =>
-          task.parentId === categoryId && (
+      return (
+        <Row>
+          <Col>
             <Row>
               <Col>
-                <Row>
-                  <Col>
-                    <Title>{categoryName}</Title>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col>
-                    <TaskList taskId={task.taskId}>{task.taskName}</TaskList>
-                  </Col>
-                </Row>
+                <Title>{categoryName}</Title>
               </Col>
             </Row>
-          )
+            <Row>
+              <Col>
+                {state.tasks.map(task => {
+                  return (
+                    categoryId === task.parentId && (
+                      <TaskSimpleView
+                        taskStatus={task.taskStatus}
+                        taskId={task.taskId}>
+                        {task.taskName}
+                      </TaskSimpleView>
+                    )
+                  )
+                })}
+              </Col>
+            </Row>
+          </Col>
+        </Row>
       )
     })
     return result
